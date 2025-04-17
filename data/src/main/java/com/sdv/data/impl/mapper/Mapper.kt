27f7571f935d17a93ba35c91a0533034835ef0c8
Dmitry Stepanov <dmitry.stepanov@magenta-technology.com>
2025-04-17 14:41:39 +0400

    added Data module

--- data/src/main/java/com/sdv/data/impl/mapper/Mapper.kt
+++ data/src/main/java/com/sdv/data/impl/mapper/Mapper.kt
@@ -4,29 +4,29 @@ import com.sdv.data.impl.entity.NodeEntity
 import com.sdv.domain.model.Node
 
 
-fun Node.toEntity() = NodeEntity(
+internal fun Node.toEntity() = NodeEntity(
     id = this.id,
     name = this.name,
     idParent = this.idParent,
     parents = listToString(this.parents)
 )
 
-fun NodeEntity.toModel() = Node(
+internal fun NodeEntity.toModel() = Node(
         id = this.id,
         name = this.name,
         idParent = this.idParent,
         parents = stringToList(this.parents)
 )
 
-fun listToString(list: List<Long>) = list.joinToString(",")
+internal fun listToString(list: List<Long>) = list.joinToString(",")
 
-fun stringToList(str: String) = str
+internal fun stringToList(str: String) = str
     .split(",")
     .toList()
     .map {
         it.toLong()
     }
 
-fun List<NodeEntity>.toModel(): List<Node> = this.map{ nodeEntity ->
+internal fun List<NodeEntity>.toModel(): List<Node> = this.map{ nodeEntity ->
     nodeEntity.toModel()
 }
